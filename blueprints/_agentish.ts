7e79e891da4de6de7e9706a5c77ce035095100e1
Rick Love <rick@toldpro.com>
2026-03-18 19:16:04 +0000

    improve agentish def

--- blueprints/_agentish.ts
+++ blueprints/_agentish.ts
@@ -1,43 +1,33 @@
-const Agentish = define.concept('Agentish');
+const Agentish = define.language('Agentish');
+const Blueprint = define.entity('Blueprint');
+
+Agentish.servesAs('AbsoluteSourceOfTruth').for(Blueprint);
+Agentish.requires('MaximumInformationDensity');
+Agentish.rejects('Comments').and('Passivity').and('WastefulObviousPhrases');
 
 const Human = define.actor('Human');
-const Agent = define.actor('Agent');
+Human.architects(Blueprint).using(Agentish);
+
 const Visualizer = define.system('Visualizer');
+Visualizer.projects(Blueprint).toHelp(Human).understand('Architecture');
 
-const Blueprint = define.entity('Blueprint');
+const Agent = define.actor('Agent');
+Agent.reads(Blueprint).toUnderstand('SystemIntent');
+
 const Codebase = define.entity('Codebase');
-const VisualGraph = define.entity('VisualGraph');
+Agent.generates(Codebase).strictlyFrom(Blueprint);
 
-Agentish.servesAs('AbsoluteSourceOfTruth');
-Agentish.requires('MaximumInformationDensity');
-Agentish.rejects('WastefulObviousPhrases');
-Agentish.rejects('RedundantDefinitions');
-Agentish.rejects('Passivity');
-Agentish.rejects('Ambiguity');
-
-when(Agentish.defines('Topology'))
-  .then(Agentish.enforces('Subject.verb(Object)'));
-
-when(Agentish.defines('Causality'))
-  .then(Agentish.enforces('when(Trigger).then(Action)'));
-
-when(Human.architects('System'))
-  .then(Human.authors(Blueprint).using(Agentish));
-
-when(Human.inspects(Blueprint))
-  .then(Visualizer.projects(Blueprint).into(VisualGraph));
-
-when(Agent.reads(Blueprint))
-  .then(Agent.generates(Codebase).strictlyFrom(Blueprint));
-
-when(Agent.encounters('LogicalGap').in(Blueprint))
-  .then(Agent.halts('Generation'))
-  .and(Agent.requestsClarificationFrom(Human).on(Blueprint));
+when(Agent.encounters('Ambiguity').in(Blueprint))
+  .then(Agent.asks(Human, 'to clarify the requirement'));
 
 when(Human.mutates(Blueprint))
   .then(Agent.discards(Codebase))
   .and(Agent.regenerates(Codebase).strictlyFrom(Blueprint));
 
-when(Human.mutates(Codebase).bypassing(Blueprint))
+Agent.measures('InformationDensity').toEvaluate('BlueprintQuality');
+
+when(Human.edits(Codebase).bypassing(Blueprint))
   .then(Agent.strikes(Codebase))
   .and(Agent.regenerates(Codebase).strictlyFrom(Blueprint));
+
+Agentish.allows('AnyFluentPhrase').toExpress('TopologyAndCausality');
